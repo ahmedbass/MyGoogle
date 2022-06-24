e50faca499025b6cc25fb02416c637b92a150919
Ahmed <Ahmedbass94@gmail.com>
2022-06-25 00:40:11 +0300

    get the search term from input and redirect to search page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
 import Image from "next/image";
 import { MicrophoneIcon, SearchIcon } from "@heroicons/react/solid";
+import { useCallback, useRef } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+  const autoFocus = useCallback((el) => el?.focus(), []);
+  const searchInputRef = useRef(null);
+  const search = (e) => {
+    const q = searchInputRef.current.value.trim();
+    if (!q) return;
+    router.push({ pathname: "/search", query: { q, type: "all" } });
+  };
+
   return (
     <div className="h-full flex flex-col justify-center items-center">
       <div className="w-full flex flex-col justify-center items-center -mt-40">
@@ -13,16 +24,22 @@ export default function Home() {
         />
 
         <div
-          className="w-full flex my-8 py-3 px-4 rounded-full items-center justify-center border max-w-[90%] 
-              hover:shadow-md focus-within:shadow-md sm:max-w-xl lg:max-w-2xl"
+          className="w-full flex my-8 py-3 px-4 rounded-full items-center justify-center border 
+            max-w-[90%] hover:shadow-md focus-within:shadow-md sm:max-w-xl lg:max-w-2xl"
         >
           <SearchIcon className="h-5 text-gray-500" />
-          <input type="text" className="flex-grow focus:outline-none mx-3" />
+          <input
+            ref={searchInputRef}
+            type="text"
+            className="flex-grow focus:outline-none mx-3"
+          />
           <MicrophoneIcon className="h-5 text-blue-500" />
         </div>
 
         <div className="flex justify-center flex-col sm:flex-row w-1/2 space-y-2 sm:space-y-0 sm:space-x-4">
-          <button className="btn">Google Search</button>
+          <button className="btn" onClick={search}>
+            Google Search
+          </button>
           <button className="btn">I&#39;m Feeling Lucky</button>
         </div>
       </div>
